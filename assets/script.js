497d4438e70b29edcbbed6044be24355b359102b
shravanibagagwde <134454320+shravanibagawde@users.noreply.github.com>
2024-09-20 18:41:20 -0400

    Big Update woohoo

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,26 @@
- console.log('Hello World!');
+console.log('Hello World!');
 
- let platesIndex = document.querySelector('.plates-index');
+function animateTitle() {
+i >= message.length - 1 ? (i = 0) : i++,
+(document.title = message[i]),
+setTimeout('animateTitle()', 1000)
+}
+var message = [
+'Shravani Bagawde', 'Shravani Bagawde', ';)']
+i = 0
+animateTitle()
 
- platesIndex.addEventListener('wheel', (event) => {
-  event.preventDefault();
+let platesIndex = document.querySelector('.plates-index');
 
-  platesIsndex.scrollBy({
-    left: event.deltaY < 0 ? -30 : 30,
-  });
-});
+const buttonLeft = document.getElementById('left');
+const buttonRight = document.getElementById('right');
+
+buttonRight.onclick = function () {
+platesIndex.scrollLeft += 250;
+};
+buttonLeft.onclick = function () {
+platesIndex.scrollLeft -= 250;
+};
 
 // const toggle = document.querySelector(".toggle")
 // const info1 = document.querySelector(".info1")
@@ -16,16 +28,6 @@
 // toggle.addEventListener("click", function () {
 //     info1.classList.toggle("invisible")
 // })
-
-const buttonRight = document.getElementById('right');
-const buttonLeft = document.getElementById('left');
-
-// buttonRight.onclick = function () {
-// platesIndex.scrollLeft += 250;
-// };
-// buttonLeft.onclick = function () {
-// platesIndex.scrollLeft -= 250;
-// };
 
     fetch('https://api.are.na/v2/channels/shravani-bagawde-plates/contents?per=100')
     .then((response) => response.json())
@@ -45,12 +47,13 @@ const buttonLeft = document.getElementById('left');
 
         let title = document.createElement('div');
         image.src = plates[i].image.display.url;
+        let altText = plates[i].description;
+        image.setAttribute("alt", 'Hiii')
         title.innerHTML = plates[i].title;
         title.setAttribute('id', 'title' + i)
         title.className = 'title';
         title.classList.add('text');
         title.classList.add('cap');
-    
         plate.appendChild(image);
         plate.appendChild(title);
 
